cf267096bf98474fc808a0e1e6d2074ae41db196
Amine MEBARKI <105977621+MEBARKI16@users.noreply.github.com>
2024-03-02 14:56:10 +0100

    update

--- src/pages/api/SendMail.js
+++ src/pages/api/SendMail.js
@@ -1,6 +1,6 @@
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend('re_3MVnwguv_57GAsi8hYqTVt5e4g8Mey5GR');
 
 export default async function handler(req, res) {
     const { body } = req;
